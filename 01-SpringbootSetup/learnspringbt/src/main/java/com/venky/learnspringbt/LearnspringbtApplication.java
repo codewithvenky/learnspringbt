cc4316fd102b3e7d78efef3f557a056b15ec85c6
abc <abc@example.com>
2021-06-21 11:15:03 +0530

    Basic working setup has been implemented in Spring boot

---
+++ 01-SpringbootSetup/learnspringbt/src/main/java/com/venky/learnspringbt/LearnspringbtApplication.java
@@ -0,0 +1,13 @@
+package com.venky.learnspringbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringbtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringbtApplication.class, args);
+	}
+
+}
---
+++ 01-SpringbootSetup/learnspringbt/src/test/java/com/venky/learnspringbt/LearnspringbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venky.learnspringbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
