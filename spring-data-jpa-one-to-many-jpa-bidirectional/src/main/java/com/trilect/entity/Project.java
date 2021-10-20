6fcdd4dc071b3bbfcb92e8652fd1c131ab263702
codewithvenky <codewithvenky@gmail.com>
2021-10-20 22:18:36 +0530

    onetoMany Simple Example

---
+++ spring-data-jpa-one-to-many-jpa-bidirectional/src/main/java/com/trilect/entity/Project.java
@@ -0,0 +1,43 @@
+package com.trilect.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+
+@Entity
+@Table(name = "t_project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+   @JoinColumn(name = "project_id",referencedColumnName ="id" )
+    @OneToMany(targetEntity = Student.class, cascade = CascadeType.ALL)
+    private List<Student> students;
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<Student> getStudents() {
+        return students;
+    }
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
